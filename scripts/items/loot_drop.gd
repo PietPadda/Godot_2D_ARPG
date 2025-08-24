e68eefdb40dd6fadb95b39a1ae01ac6dd5d9db4b
FROGPAD <pietergdewaal@gmail.com>
2025-08-24 21:19:46 +0200

    feat(inventory): implement inventory component and loot pickup logic

--- scripts/items/loot_drop.gd
+++ scripts/items/loot_drop.gd
@@ -5,6 +5,24 @@ extends Area2D
 # scene nodes
 @onready var sprite: Sprite2D = $Sprite2D
 
+# This will store the item data for this specific drop.
+var item_data: ItemData
+
 # This function will be called by whatever spawns the loot.
-func initialize(item_data: ItemData) -> void:
-	sprite.texture = item_data.texture
+func initialize(data: ItemData) -> void:
+	self.item_data = data
+	sprite.texture = data.texture
+
+
+func _on_body_entered(body: Node2D) -> void:
+	# Check if the body that entered is the player.
+	if body.is_in_group("player"):
+		# Get the player's inventory component.
+		var inventory_component: InventoryComponent = body.get_node("InventoryComponent")
+
+		# Try to add our item to their inventory.
+		var picked_up = inventory_component.add_item(item_data)
+
+		# If the item was successfully picked up, destroy the loot drop.
+		if picked_up:
+			queue_free()
