86ae84d82894b6eb6eab95fc2eb81ed0842b998a
FROGPAD <pietergdewaal@gmail.com>
2025-08-26 12:28:03 +0200

    feat: add follow camera and improve health bar UI

--- scripts/ui/hud.gd
+++ scripts/ui/hud.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 
 # scene nodes
 @onready var health_bar: ProgressBar = $PlayerHealthBar
+@onready var health_label: Label = $PlayerHealthBar/HealthLabel # child of healthbar
 @onready var inventory_panel = $InventoryPanel
 
 func _ready() -> void:
@@ -13,6 +14,9 @@ func _ready() -> void:
 		var player_stats = player.get_node("StatsComponent")
 		# Connect our UI update function to the player's signal.
 		player_stats.health_changed.connect(on_player_health_changed)
+		
+		# Manually update the bar once on startup to get the initial value.
+		on_player_health_changed(player_stats.current_health, player_stats.stats_data.max_health)
 		
 		# Connect to the new inventory signal.
 		var player_inventory = player.get_node("InventoryComponent")
@@ -28,3 +32,5 @@ func _unhandled_input(event: InputEvent) -> void:
 func on_player_health_changed(current_health: int, max_health: int) -> void:
 	health_bar.max_value = max_health
 	health_bar.value = current_health
+	# Update the label's text with the current and max health.
+	health_label.text = "%d / %d" % [current_health, max_health]
