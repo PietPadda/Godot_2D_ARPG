b2f14dc67b8cd57995ca5cfeecfd6c0091659bdd
FROGPAD <pietergdewaal@gmail.com>
2025-08-28 22:23:51 +0200

    fix(player): fix chase state freeze and range calculation

--- scripts/player/states/player_chase_state.gd
+++ scripts/player/states/player_chase_state.gd
@@ -1,12 +1,10 @@
 # player_chase_state.gd
 # The state for moving towards a target to get within attack range.
 class_name PlayerChaseState
-extends State
+extends PlayerState # Make sure it extends PlayerState
 
 var target: Node2D
 
-@onready var player: CharacterBody2D = get_owner()
-@onready var movement_component: PlayerMovementComponent = player.get_node("PlayerMovementComponent")
 @onready var attack_component: AttackComponent = player.get_node("AttackComponent")
 @onready var animation_component: AnimationComponent = player.get_node("AnimationComponent")
 
@@ -24,23 +22,24 @@ func enter() -> void:
 func process_physics(delta: float) -> void:
 	# First, check if our target still exists.
 	if not is_instance_valid(target):
-		print("Exiting Chase State")
 		state_machine.change_state(States.PLAYER_STATE_NAMES[States.PLAYER.IDLE]) # just idle if invalid target
 		return # early exit
 
 	# Continuously update the movement target in case the target moves.
 	movement_component.set_movement_target(target.global_position)
 
-	# Check the distance to the target.
+	# Check distance using our new, correct stat calculator.
 	var distance_to_target = player.global_position.distance_to(target.global_position) 
-	var attack_range = attack_component.attack_data.range # range calc
+	var attack_range = stats_component.get_total_stat("range") # Correctly call the calculator
 
 	if distance_to_target <= attack_range: # if we're in range
-		# We are in range! Stop moving.
-		movement_component.set_movement_target(player.global_position) # update target
+		# We are in range! Stop moving and transition to Attack.
+		player.velocity = Vector2.ZERO # Explicitly stop
 		
 		# Pass the target to the AttackState and transition.
 		var attack_state = state_machine.states["attack"] # attackstate
 		attack_state.target = target # pass the target to attack
-		print("Exiting Chase State")
 		state_machine.change_state(States.PLAYER_STATE_NAMES[States.PLAYER.ATTACK]) # and attack
+	else:
+		# If we're not in range, we need to move! This was the missing piece.
+		perform_movement()
