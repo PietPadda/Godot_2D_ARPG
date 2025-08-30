0b034e300789d799c59fb180b3b9f0b874ca492d
FROGPAD <pietergdewaal@gmail.com>
2025-08-30 22:27:59 +0200

    refactor(player): finalize grid movement with a self-contained component and simplified FSM states

--- scripts/player/states/player_move_state.gd
+++ scripts/player/states/player_move_state.gd
@@ -6,41 +6,25 @@ extends PlayerState # Changed from 'State'
 # Scene referenes needed for move state
 @onready var grid_movement_component: GridMovementComponent = player.get_node("GridMovementComponent")
 
-# This will hold the array of world positions we need to walk through.
 var move_path: PackedVector2Array = []
+var final_destination_tile: Vector2i
 
 func enter() -> void:
-	# Remove the first point, which is the player's current location.
-	# This prevents trying to first walk to it's starting position
 	if not move_path.is_empty():
-		move_path.remove_at(0)
-
+		final_destination_tile = Grid.world_to_map(move_path[move_path.size() - 1])
+	
+	grid_movement_component.move_along_path(move_path)
 	# When we enter, start listening for the component to finish a step.
-	grid_movement_component.move_finished.connect(_on_move_finished)
+	grid_movement_component.path_finished.connect(_on_path_finished)
 	player.get_node("AnimationComponent").play_animation("Move")
-	# Immediately try to move to the first tile in our path.
-	_move_to_next_tile()
 	
 func exit() -> void:
 	# IMPORTANT: Disconnect the signal when we leave this state to prevent bugs.
-	grid_movement_component.move_finished.disconnect(_on_move_finished)
+	if grid_movement_component.path_finished.is_connected(_on_path_finished):
+		grid_movement_component.path_finished.disconnect(_on_path_finished)
 
-# This is the heart of our path-following loop.
-func _move_to_next_tile() -> void:
-	# stop moving if no path
-	if move_path.is_empty():
-		state_machine.change_state(States.PLAYER_STATE_NAMES[States.PLAYER.IDLE])
-		return
-	
-	# otherwise, get next path point and move
-	var next_pos = move_path[0]
-	move_path.remove_at(0)
-	grid_movement_component.move_to(next_pos)
-
-# Called by the component's signal when a single tile move is done.
-func _on_move_finished() -> void:
-	# When we arrive, simply try to move to the next one.
-	_move_to_next_tile()
+func _on_path_finished() -> void:
+	state_machine.change_state(States.PLAYER_STATE_NAMES[States.PLAYER.IDLE])
 
 # process_input handles discretse events like casting.
 func process_input(event: InputEvent) -> void:
@@ -53,11 +37,11 @@ func process_input(event: InputEvent) -> void:
 func process_physics(_delta: float) -> void:
 	# We check for new move commands ONLY if the component is NOT busy.
 	if Input.is_action_pressed("move_click"):
-		var end_pos = Grid.world_to_map(player.get_global_mouse_position())
-		var start_pos = Grid.world_to_map(player.global_position)
-		var new_path = Grid.find_path(start_pos, end_pos)
-		
-		# Only update if a valid path was found.
-		if not new_path.is_empty():
-			self.move_path = new_path
-			_move_to_next_tile()
+		var current_mouse_tile = Grid.world_to_map(player.get_global_mouse_position())
+		if current_mouse_tile != final_destination_tile:
+			var start_pos = Grid.world_to_map(player.global_position)
+			var new_path = Grid.find_path(start_pos, current_mouse_tile)
+			# Only update if a valid path was found.
+			if not new_path.is_empty():
+				self.final_destination_tile = current_mouse_tile
+				grid_movement_component.move_along_path(new_path)
