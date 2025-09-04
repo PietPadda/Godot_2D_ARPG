1a95f4185d5ee8c161761eda6b2a48d6a09a6def
FROGPAD <pietergdewaal@gmail.com>
2025-09-04 18:30:41 +0200

    fix(system): prevent health restoration on scene transition

--- data/save_data.gd
+++ data/save_data.gd
@@ -5,6 +5,11 @@ extends Resource
 # Description
 ## A data container for all save game properties.
 
+# Always Persisting Stats
 @export var player_stats_data: CharacterStats
 @export var player_inventory_data: InventoryData
 @export var player_equipment_data: EquipmentData
+
+# Live Stats for Scene Transitions
+var current_health: int
+var current_mana: int
